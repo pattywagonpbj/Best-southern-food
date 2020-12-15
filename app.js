5cfd52cd2897ca1a31c540d76458eb8dc5a4727a
pattywagonpbj <patrick.jarrett11@gmail.com>
2020-12-14 19:56:03 -0800

    update website

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
     var username = prompt("Please enter your name");
     document.write("Welcome, " + username);
+
+    function asktofindbreakfastspot(){
+        var breakfastQuestion = confirm("Would you like to find a good breakfast spot?");
+        if (breakfastQuestion==true){
+            alert("Lets Go!");
+        } else if (breakfastQuestion==false){
+            alert("Learn to love breakfast!");
+            window.location.replace('https://vinepair.com/articles/best-wines-2020/');
+        }
+    }
+
