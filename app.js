29d0d1a551060995dee7451678d821138fb24048
pattywagonpbj <patrick.jarrett11@gmail.com>
2020-12-14 20:15:37 -0800

    update website

--- app.js
+++ app.js
@@ -11,3 +11,30 @@
         }
     }
 
+    function askUserHowManyStatesCookASouthernBreakfast(){
+        var userAnswer = prompt ('How many states cook a southern breakfast?');
+        while (userAnswer != 4){
+            userAnswer = prompt('Try again!');
+        } if (userAnswer = 4){
+            alert('You got it!')
+        }
+    }
+
+    function placeCurrentDate(){
+        var todayDate = new Date(); 
+        document.write(todayDate);
+    }
+
+    function numberGame(){
+        var gameAnswer = prompt('Pick a number between 1 and 100. You get 12 guesses, so make them count!');
+        for (var i = 0; i < 60; i++){
+            if(gameAnswer === '23'){
+                alert('You got it right! Congratulations!');
+                break;
+            } 
+            if(i === 12 && gameAnswer != 4){
+                alert('Well that was clearly not the answer, better luck next time champ!'); 
+            }
+            gameAnswer = prompt(`Pick a number between 1 and 100. You have ${23-i} guesses!`); 
+        }
+    }
